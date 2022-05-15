14ae45f07f564e483fa6cde3da561d6fc1e4bbb0
Justin Fleming <fezzygo@gmail.com>
2022-05-15 15:53:20 -0400

    renamed userLowerCase function to useCase, add uppercase question to it

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -5,46 +5,51 @@ var passwordType = [];
 
 
 // Add number array
-var number = [0,1,2,3,4,5,6,7,9];
+var number = [0, 1, 2, 3, 4, 5, 6, 7, 9];
 
 // Add alphabet array
-var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
 // Add special char array
-var special = ["!", "@", "#", "$", "%", "^", "&", "(", ")", "]", "^","~", "*", "?", ":"];
+var special = ["!", "@", "#", "$", "%", "^", "&", "(", ")", "]", "^", "~", "*", "?", ":"];
 
 // function to get passlength
-function getlength(){
+function getlength() {
   while (passwordLength < 8 || passwordLength > 128) {
     passwordLength = prompt("Enter the length of the password");
   }
   return;
 }
 
-function useLowerCase(){
-  var userlower = prompt("Use lowercase charaters? y for Yes or n for No");
-  while ((userlower != "n") && (userlower != "y")) {
-    userlower = prompt("Use lowercase charaters? y for Yes or n for No");
+// function to chose weather or not to use lower case
+function useCase(caseType) {
+
+  var useCase = prompt("Use " + caseType + "case charaters? y for Yes or n for No");
+  while ((useCase != "n") && (useCase != "y")) {
+    userlower = prompt("Use " + caseType + "case charaters? y for Yes or n for No");
   }
-  if (userlower === "y") {
+  if (caseType === "lower" && useCase === "y") {
     passwordType.push("lower");
+    console.log(passwordType);  // Testing
+  } else if (caseType === "upper" && useCase === "y") {
+    passwordType.push("upper");
     console.log(passwordType);  // Testing
   }
 }
-
 
 // create generatePassword function
 function generatePassword() {
   // get password length
   getlength();
 
-  useLowerCase();
+  useCase("lower");
+  useCase("upper");
 
-    // for loop to count length
+  // for loop to count length
 
-      // for loop to randomly choose a char type
-         // for loop to choose a char from the array
-         // add chosen char to password array
+  // for loop to randomly choose a char type
+  // for loop to choose a char from the array
+  // add chosen char to password array
 
   return passwordLength; //Testing
 }
