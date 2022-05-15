344d2519ed6b0600d472a4a27b8b95f8cc11e3b6
unknown <fezzygo@gmail.com>
2022-05-14 23:51:07 -0400

    added arrays for each type

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -1,8 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
 // Add number array
+var number = [0,1,2,3,4,5,6,7,9];
+
 // Add alphabet array
+var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
 // Add special char array
+var special = ["!", "@", "#", "$", "%", "^", "&", "(", ")", "]", "^","~", "*", "?", ":"];
 
 // function to get passlength
 
