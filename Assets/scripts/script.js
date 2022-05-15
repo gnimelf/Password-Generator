adaebe1e7ec840f81ede9daac9f9379b2f065968
unknown <fezzygo@gmail.com>
2022-05-14 23:41:58 -0400

    Added psudo code to project

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -1,5 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+// Add number array
+// Add alphabet array
+// Add special char array
+
+// function to get passlength
+
+// create generatePassword function
+  // for loop to count length
+      // for loop to randomly choose a char type
+         // for loop to choose a char from the array
+         // add chosen char to password array
+
 
 // Write password to the #password input
 function writePassword() {
